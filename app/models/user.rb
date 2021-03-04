e1b4822920ff2f20f502b794d0ad935c763c2598
saikalcom08 <saikalcom08@gmail.com>
2021-03-04 15:22:38 +0000

    Adding user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
     has_many :articles
+    validates :username, presence: true, 
+                         uniquenness: { case_sensitive: false }, 
+                         length: { minimum: 3, maximum: 25 }
+    VALID_EMAIL_REGEX = ''                     
+    validates :email, presence: true, 
+                      uniquenness: { case_sensitive: false }, 
+                      length: { maximum: 105 },
+                      format: { with: VALID_EMAIL_REGEX }
 end
